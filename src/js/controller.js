48b3e6ac446369185fa1a938fa93dba494b9f480
duraanos <duran364@anadolu.edu.tr>
2024-09-04 12:44:11 +0300

    Select the necessary elements

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,2 +1,17 @@
 'use strict';
 
+const totalBalance = document.querySelector('.total-balance p');
+const income = document.querySelector('.income p');
+const expense = document.querySelector('.expense p');
+
+const transactionName = document.querySelector('.transaction p:nth-child(1)');
+const transactionAmount = document.querySelector('.transaction p:nth-child(2)');
+const transactionDate = document.querySelector('span');
+
+const inputName = document.querySelector('.name input');
+const inputAmount = document.querySelector('.amount input');
+const inputDate = document.querySelector('.date input');
+
+const btnIncome = document.querySelector('.btn-income');
+const btnExpense = document.querySelector('.btn-expense');
+const btnSubmit = document.querySelector('button[type="submit"]');
